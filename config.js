566b51a1e6c8de8b7627241fc91a8789664ca08d
joshuazwoody-glitch <joshuazwoody@gmail.com>
2026-04-14 11:53:26 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  host: 'donutsmp.net',
+  port: 25565,
+  username: 'YOUR_BOT_ALT_USERNAME_HERE',   // ← CHANGE THIS to your alt account username
+  version: '1.21',                          // Works on current Donut SMP
+  auth: 'microsoft',                        // Use 'offline' only if server allows cracked accounts
+
+  viewerPort: process.env.PORT || 3000
+};
